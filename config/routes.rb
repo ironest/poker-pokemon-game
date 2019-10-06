8e32fe035e73f433a6f99202740939c21f6ffc24
ironest <ironest@gmail.com>
2019-10-06 23:12:59 +1100

    Created basic routes for test

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/robots", to: "robots#index", as: "robots"
+  post "/robots", to: "robots#create"
+  get "/robots/new", to: "robots#new", as: "new_robot"
+  get "/robots/refresh", to: "robots#refresh", as: "refresh"
+  get "/robots/:id", to: "robots#show", as: "robot"
+
+
 end
