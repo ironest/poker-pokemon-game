246db3f4846a89a0a6455c6ff146f2072f87f3d0
ironest <ironest@gmail.com>
2019-10-08 01:29:12 +1100

    Updated routes to receive new robots

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get "/robots", to: "robots#index", as: "robots"
-  post "/robots", to: "robots#create"
-  get "/robots/new", to: "robots#new", as: "new_robot"
+  # post "/robots", to: "robots#create"
   get "/robots/refresh", to: "robots#refresh", as: "refresh"
+  get "/robots/new/:id", to: "robots#new", as: "new_robot"
+  get "/robots/delete/:id", to: "robots#delete", as: "delete_robot"
   get "/robots/:id", to: "robots#show", as: "robot"
 
 
